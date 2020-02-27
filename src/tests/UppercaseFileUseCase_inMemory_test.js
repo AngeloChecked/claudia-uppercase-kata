4725a5ce7f98f4757ebcecbf98e0a0a509dab764
Angelo Ceccato <angelo.ceccato@it.clara.net>
2020-02-27 23:09:01 +0100

    add in memory file system test

---
+++ src/tests/UppercaseFileUseCase_inMemory_test.js
@@ -0,0 +1,55 @@
+const UppercaseFileUseCase = require('../domain/UppercaseFileUseCase');
+const UppercaseStreamConverter = require('../delivery/UppercaseStreamConverter'); 
+const { Readable, Writable } = require("stream");
+const assert = require('assert')
+
+
+class InMemoryFileSystem {
+   
+    constructor(inputString){
+        this.inputString = inputString;
+    }
+
+    chunks=[];
+
+    get inMemoryStreamResult(){
+        return Buffer.concat(this.chunks).toString();
+    }
+
+    readAsStream = () => Readable.from([this.inputString])
+
+    writeFromStream(fileName, bucketName, inputStream, contextCallback) {
+        let outputStream = new Writable({
+            write: (chunk, encoding, streamCallback) => this.chunks.push(chunk) 
+        });
+        inputStream.pipe(outputStream);
+        inputStream.on('finish', contextCallback)
+    }
+}
+
+describe("In Memory File System Test", () => {
+
+    let inputText = 'Lorem ipsum dolor sit amet.';
+    let fileSystem = new InMemoryFileSystem(inputText);
+
+    it("Output text should be uppercased",async () => {
+        await new Promise((resolve, reject) => {
+            let context = { done: resolve };
+            let usecase = new UppercaseFileUseCase(
+                    new UppercaseStreamConverter(), fileSystem, context, null
+                );
+            let domainEvent = {
+                fileName: null,
+                bucketName: 'buketName'
+            };
+
+            usecase.run(domainEvent)
+        });
+
+        let expectedOutputText = 'LOREM IPSUM DOLOR SIT AMET.';
+        assert.equal(expectedOutputText, fileSystem.inMemoryStreamResult)
+    });
+
+});
+
+
